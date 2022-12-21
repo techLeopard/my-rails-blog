c465fc8ef97c6c05daeb5ea7822a23221158633b
Ed Satie <adilet.satylganov@gmail.com>
2022-12-21 22:02:31 +0600

    Change root

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root to: "articles#index" 
+  root to: "main#index" 
   get "about", to: "about#index"
 
   resources :articles
